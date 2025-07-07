4af84846f83966ee6f4e5004b7dfc730db087abd
Jatin kushwaha <109662028+Jk319@users.noreply.github.com>
2025-07-07 15:24:14 +0530

    Delete brain_tumor_detection directory

--- brain_tumor_detection/brain_model.joblib.py
+++
@@ -1,5 +0,0 @@
-
-import joblib
-
-# Save the model as a .joblib file
-joblib.dump(model, "brain_model.joblib")
--- brain_tumor_detection/utils.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def preprocess_image(image):
-    image = image.resize((128, 128))  # Match your training size
-    image = image.convert('L')  # Grayscale
-    image = np.array(image).flatten()  # Flatten to 1D if classical ML
-    image = image.reshape(1, -1)  # Model expects 2D (samples, features)
-    return image
