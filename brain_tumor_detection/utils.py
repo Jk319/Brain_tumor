d4253051f7d7c83c6906ffc3d9735ea5d7853971
Jatin kushwaha <109662028+Jk319@users.noreply.github.com>
2025-07-10 11:45:17 +0530

    Delete brain_tumor_detection directory

--- brain_tumor_detection/utils.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def preprocess_image(image):
-    image = image.resize((128, 128))  # Match training input shape
-    image = image.convert('L')        # Convert to grayscale
-    image = np.array(image).flatten() # Flatten image
-    return image.reshape(1, -1)       # Return as model input
