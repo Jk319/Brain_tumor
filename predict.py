bb65426a24a139c45fe273d1727f2f593456381f
Jatin kushwaha <109662028+Jk319@users.noreply.github.com>
2025-07-17 08:41:23 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ model = load("model.pkl") # or whatever your model path is
 
 def predict_image(path):
     img = cv2.imread(path)
-    img = cv2.resize(img, (150, 150))
+    img = cv2.resize(img, (100, 100))
     img = img.flatten().reshape(1, -1)
     prediction = model.predict(img)
     return "Tumor" if prediction[0] == 1 else "No Tumor"
